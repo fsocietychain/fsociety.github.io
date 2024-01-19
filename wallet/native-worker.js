04fcbf0a92185872669aaf2f9ebf29beee202602
fsociety (FSC) <132659431+fsocietychain@users.noreply.github.com>
2024-01-19 19:16:06 +0100

    update

--- wallet/native-worker.js
+++
@@ -1,1 +0,0 @@
-self.addEventListener("install",(function(e){console.log("[ServiceWorker] Install"),self.skipWaiting()})),self.addEventListener("activate",(e=>{console.log("[ServiceWorker] Activated"),self.clients.claim()}));
